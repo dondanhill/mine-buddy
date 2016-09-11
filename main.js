c10b19c11e39cb8b2418a9f164d734bed6d28afb
dondanhill <dondanhill@googlemail.com>
2016-09-11 10:52:54 +0200

    extra styling, game stops correctly

--- main.js
+++ main.js
@@ -1,30 +1,53 @@
+var startTime;
+var timeOutId;
+var started = false;
+
 window.onload = function () {
     var canvas = document.getElementById('canvas'),
         context = canvas.getContext('2d'),
-        width = canvas.width = window.innerWidth,
-        height = canvas.height = window.innerHeight
-
+        width = 0,
+        height = 0;
+    
     canvas.addEventListener('mousedown', function (event) {
-        if (event.button === 0) {
-            grid.clicked(event.clientX, event.clientY);
-        } else {
-            grid.rightClicked(event.clientX, event.clientY);
+        if (!started) {
+            startTime = new Date().valueOf();
+            timeOutId = setInterval(function() {
+                var d = new Date().valueOf();
+                document.getElementById('time').innerHTML = Math.floor((d - startTime) / 1000);
+                console.log(d);
+            }, 1000); 
+            started = true;
         }
+        if (!grid.finished) {
+            if (event.button === 0) {
+                grid.clicked(event.clientX, event.clientY);
+            } else {
+                grid.rightClicked(event.clientX, event.clientY);
+            } 
+            if (grid.finished) {
+                var d = new Date().valueOf();
+                clearTimeout(timeOutId);
+                document.getElementById('time').innerHTML = (d - startTime) / 1000;
+            }    
+        }
+        document.getElementById('bombs').innerHTML = grid.flags;
+        document.getElementById('total').innerHTML = grid.bombs.length; 
     });
     canvas.addEventListener('contextmenu', function(event) {
         event.preventDefault();
         return false;
     })
     // begin
-    var grid = new Grid(12, 8, 30, context);
+    var grid = new Grid(21, 9, 30, context);
+    width = canvas.width = grid.w * grid.size;
+    height = canvas.height = grid.h * grid.size;
     grid.draw();
-    console.log('Grid:', grid.w, grid.h, grid.h * grid.w);
-    console.log('number of bombs: ' + grid.bombs.length);
 };
 
 function Grid(w, h, size, context) {
     this.boxes = [];
     this.bombs = [];
+    this.flags = 0;
     this.w = w;
     this.h = h;
     this.size = size;
@@ -91,7 +114,9 @@ function Grid(w, h, size, context) {
             flags += (box.flagged) ? 1 : 0;
             bombs += (box.hasBomb && box.clicked);
         });
+        this.flags = flags;
         if (bombs) {
+            this.finished = true;
             this.bombs.forEach(function(bomb) {
                 bomb.setClicked();
             })
@@ -99,6 +124,7 @@ function Grid(w, h, size, context) {
         console.log('bombs', this.bombs.length, ', flags:', flags);
         if (this.boxes.length === uncovered + this.bombs.length) {
             console.log('looks like you have won! :)');
+            this.finished = true;
         } 
     }
 
